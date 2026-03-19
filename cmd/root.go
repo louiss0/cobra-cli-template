88da198157fa3dd7bf532b8694aa6740315165ae
louiss0 <louisshelton0@gmail.com>
2026-03-18 20:04:55 -0400

    refactor(cmd): test write output function
    The root command's `RunE` function now utilizes `output.WriteModeAwareOutput` for more flexible
    output management.
    This change aligns with the project's guidelines for handling output during development and
    production.
    The test suite has also been updated to reflect this change.

--- cmd/root.go
+++ cmd/root.go
@@ -1,73 +1,41 @@
-/*
-Copyright © 2025 Shelton Louis
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package cmd
 
 import (
 	"fmt"
-	"os"
 
+	"github.com/louiss0/cobra-cli-template/output"
 	"github.com/spf13/cobra"
 )
 
 func NewRootCmd() *cobra.Command {
 
+	var Args struct {
+		Info string
+	}
+
 	cmd := &cobra.Command{
 		Use:   "cli",
-		Short: "A brief description of your application",
-		Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+		Short: "Build CLI applications with Cobra and Go",
+		Long: `A starter template for building maintainable Cobra applications.
+The template is organized for test-driven development using Ginkgo and
+Testify assertions.`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-		// Uncomment the following line if your bare application
-		// has an action associated with it:
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) > 0 {
+				Args.Info = args[0]
+				return output.WriteModeAwareOutput(cmd, fmt.Sprintf("Root here %s", Args.Info))
 
-			fmt.Fprintf(cmd.OutOrStdout(), "Root here")
+			}
 
-			return nil
+			return output.WriteModeAwareOutput(cmd, "Root here")
 		},
 	}
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// cmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mini-clis.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	cmd.MarkFlagsMutuallyExclusive()
 
 	return cmd
 }
 
-var rootCmd = NewRootCmd()
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+func Execute() error {
+	return NewRootCmd().Execute()
 }
--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,42 +1,31 @@
 package cmd_test
 
 import (
-	"github.com/brianvoe/gofakeit/v7"
 	"github.com/louiss0/cobra-cli-template/cmd"
 	. "github.com/onsi/ginkgo/v2"
 )
 
-// This function executes a cobra command with the given arguments and returns the output and error.
-// It sets the output and error buffers for the command, sets the arguments, and executes the command.
-// If there is an error, it returns an error with the error message from the error buffer.
-// If there is no error, it returns the output from the output buffer.
-// It's used to test the cobra commands.
-// When you use this function, make sure to pass the root command and any arguments you want to test.
-// The first argument after the rootCmd is any sub command or flag you want to test.
+var _ = Describe("Root Command", func() {
 
-var rootCmd = cmd.NewRootCmd()
+	It("runs with args", func() {
 
-var _ = Describe("Cmd", func() {
-
-	It("should be able to run", func() {
-
-		// The "" needs to be passed as an argument to the executeCmd function with rootCmd
-		// If not then there will be an error
-		// This is because gingko will pass a flag to the command to indicate that it is running in a test environment
-		output, err := executeCmd(rootCmd, "")
+		output, err := executeCmd(cmd.NewRootCmd(), "info")
 
 		assert.NoError(err)
+		assert.Equal("Root here info", output)
+	})
 
+	It("runs successfully", func() {
+		output, err := executeCmd(cmd.NewRootCmd())
+
+		assert.NoError(err)
 		assert.Equal("Root here", output)
-
 	})
 
-	It("should give me a random number", func() {
+	It("shows help information", func() {
+		output, err := executeCmd(cmd.NewRootCmd(), "--help")
 
-		number := gofakeit.Number(1, 100)
-
-		assert.True(number >= 1 && number <= 100)
-
+		assert.NoError(err)
+		assert.Contains(output, "starter template for building maintainable Cobra applications")
 	})
-
 })
